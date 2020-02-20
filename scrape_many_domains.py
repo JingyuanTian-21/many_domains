6b2e8b7beeb6408c8da8aef75125828d4768df2b
JingyuanTian-21 <59710644+JingyuanTian-21@users.noreply.github.com>
2020-02-20 15:54:01 -0500

    Add files via upload

---
+++ scrape_many_domains.py
@@ -0,0 +1,41 @@
+import pandas
+import os
+from urllib.request import urlopen, Request
+import time
+from tld import get_tld, get_fld
+
+if not os.path.exists("domain_html"):
+	os.mkdir("domain_html")
+
+df = pandas.read_csv("domain.csv")
+
+#print(df)
+
+for link in df['domain_name']:
+	# print("original" + link)
+	# print("fld version:", get_fld("https://" + link))
+
+	f = open('domain_html/' + link, "wb")
+	try:
+		print("Downloading (1st):", link)
+		headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.3'}
+		fld_link = get_fld("https://" + link)
+		request_link = "https://" + fld_link
+		req = Request(url = request_link, headers = headers)
+		response = urlopen(req)
+		html = response.read()
+		f.write(html)
+		f.close()
+	except:
+		print("Downloading (2nd):", link)
+		headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.3'}
+		fld_link = get_fld("https://" + link)
+		request_link = "https://" + fld_link
+		req = Request(url = request_link, headers = headers)
+		response = urlopen(req)
+		html = response.read()
+		f.write(html)
+		f.close()
+	time.sleep(10)
+
+
